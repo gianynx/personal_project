7e70b2dd56d9fdc4f32ae0c288bbbf6ad5a52cd1
Gianmarco Incocciati <minusgian@gmail.com>
2023-10-21 12:36:56 +0200

    create stores table & dbSeeder.php

---
+++ config/dbSeeder.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+    "stores" => [
+        [
+            "name" => "Round Midnight Dischi",
+            "address" => "Via di S. Orsola, 10",
+            "logo" => "round_midnight_dischi.png",
+            "description" => "La collezione è frutto di anni di ricerche per cantine, mercatini, fallimenti, promozioni ed è composta principalmente da dischi in vinile, ma anche cd, dvd, VHS, musicassette, nuovi ed usati, tutti  archiviati nel catalogo online."
+        ],
+
+        [
+            "name" => "Welcome To The Jungle - Record Store",
+            "address" => "Via Monte Zebio, 44A",
+            "logo" => "welcome_to_the_jungle.png",
+            "description" => "Il negozio si pone al servizio degli appassionati offrendo la vendita e l'assistenza di apparecchiature testate e garantite, dai giradischi, agli amplificatori, dai registratori alle casse acustiche, vintage e nuovi."
+        ],
+
+        [
+            "name" => "Millerecords",
+            "address" => "Via Merulana, 91",
+            "logo" => "millerecords.png",
+            "description" => "Varcando le porte di Millerecords, storico negozio di dischi a Roma, si respira un odore di cultura, di vinili, di un mondo di musica che ha accompagnato il cammino dell'Uomo. Dal Canto Gregoriano alla musica del Novecento con  tutte le ramificazioni dell'albero della Musica: Blues, Jazz, Soul, Rock e sottogeneri, Fusion, Disco, Punk, New Wave e molte altre sviluppatesi dalla stessa Radice. In questo sacro Tempio, tra i negozi di dischi a Roma il più longevo, potrete trovare ogni tipo di Alchimia Musicale: dischi in vinile 33-45-78 rpm, cd nuovi e usati, libri di musica, Rarità di Classica, Jazz, Rock e non solo."
+        ],
+
+        [
+            "name" => "Ultrasuoni Records Roma",
+            "address" => "Via dei Marsi, 24a",
+            "logo" => "ultrasuoni.jpg",
+            "description" => "Ultrasuoni Records is a record shop based in Rome specialized in electronic music."
+        ],
+
+        [
+            "name" => "Blutopia Roma",
+            "address" => "Via del Pigneto, 116",
+            "logo" => "blutopia.jpg",
+            "description" => "Vendita dischi (vinile/cd), libri, video (nuovi e usati), spazio ascolto, lettura e consultazione, lab."
+        ],
+
+        [
+            "name" => "Il Mangiadischi Roma",
+            "address" => "Via Luciano Manara, 22",
+            "logo" => "il_mangiadischi.jpg",
+            "description" => "Prezioso e storico negozio di dischi a Trastevere. Musica ricercata con la possibilità di bere una birra in compagnia."
+        ],
+
+        [
+            "name" => "Pink Moon",
+            "address" => "Via Antonio Paninotti, 5",
+            "logo" => "pink_moon.png",
+            "description" => "Un viaggio indietro nel tempo. Valanghe di vinili e cd. Veramente un tempio della musica. Dieci minuti di cammino dalla stazione di Trastevere. Vi aspettiamo!"
+        ],
+
+        [
+            "name" => "Disco+",
+            "address" => "Via dei Platani, 37",
+            "logo" => "disco_plus.jpg",
+            "description" => "Da Disco+ puoi trovare una vasta scelta di novità e un ricco catalogo in LP, CD, DVD e rarità ital."
+        ],
+
+        [
+            "name" => "Ace Records Roma",
+            "address" => "Via Veglia, 29",
+            "logo" => "ace_records.jpg",
+            "description" => "Ace Records is a record shop and label based in Rome, IT. We focus on Heavy Metal, Hard Rock, Pop Rock, Jazz and Classical on vinyl, tape, CD new and used."
+        ],
+
+        [
+            "name" => "I Want To Believe Recordshop",
+            "address" => "Via dei Quattro Cantoni, 10a",
+            "logo" => "i_want_to_believe_records.jpg",
+            "description" => "We are based in Roma (Italy). Close to Termini Station and tube Metro B. Specialized in all the faces of electronic music. Also selection of alternative rock, contemporary jazz and hip hop. New and 2nd hand. If you want to order by email send your order here iwtbrecords@gmail.com."
+        ],
+
+        [
+            "name" => "DischixFiaschi",
+            "address" => "Via Valsolda, 57",
+            "logo" => "dischi_x_fiaschi.jpg",
+            "description" => "In piena pandemia COVID-19, nasce il primo Record&Wine Shop di Montesacro: un angolo underground. Vi aspettiamo!"
+        ],
+
+        [
+            "name" => "Discoteca Laziale",
+            "address" => "Via Giovanni Giolitti, 263",
+            "logo" => "discoteca_laziale.jpg",
+            "description" => "Discoteca Laziale. Musica, libri, entertainment. Storico negozio di dischi, nato a Roma nel 1967."
+        ],
+
+        [
+            "name" => "Chicken Records - Music Shop",
+            "address" => "Via Rovigno d'Istria, 14a",
+            "logo" => "chicken_records.png",
+            "description" => "Chicken Records è un negozio di dischi situato in Via Rovigno d'Istria 14/a, nel quartiere di Villa Gordiani, adiacente al parco omonimo e alla storica borgata. Come ogni negozio di dischi che si rispetti, la passione per la musica è stata la scintilla che ha portato alla luce questo spazio, dentro al quale potrete trovare, tra nuovo e usato, un vasto assortimento di generi e formati, con migliaia tra LP, 7″, Cd, Audiocassette, Libri e Dvd musicali."
+        ],
+
+        [
+            "name" => "Goody Music",
+            "address" => "Via Flaminia 23, angolo via Beccaria",
+            "logo" => "goody_music.jpg",
+            "description" => "From headphones to speakers and then mixers, controlers, laser, mirror balls, audio card, flight cases, in conclusion everyone's able to manage a consolle should stop by here. In case you are newbie, in Goody Music you can find secondhand stuff, so that you don't need to slash your wrists while waiting for the fame."
+        ],
+
+        [
+            "name" => "Groove Lab Records",
+            "address" => "Via Luigi Vanvitelli, 39",
+            "logo" => "groove_lab_records.jpg",
+            "description" => "Giovane negozio di dischi e vinili indipendente a Testaccio, specializzato in musica elettronica di ogni tipo."
+        ]
+    ]
+];
